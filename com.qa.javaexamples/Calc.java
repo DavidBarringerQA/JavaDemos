b9436c5b604087738c35c6a25796b07c161bbf0f
DavidBarringerQA <DBarringer@qa.com>
2021-06-23 12:08:41 +0100

    Added conditionals to Calc and Results

--- com.qa.javaexamples/Calc.java
+++ com.qa.javaexamples/Calc.java
@@ -3,14 +3,9 @@ package com.qa.javaexamples;
 public class Calc {
   public static void main(String[] args) {
     System.out.println(
-        "2 + 3 = "
-            + add(2, 3)
-            + "\n7 - 5 = "
-            + sub(7, 5)
-            + "\n4 * 3 = "
-            + mul(4, 3)
-            + "\n25 / 5 = "
-            + div(25, 5));
+        "2 + 3 = " + add(2, 3) + "\n7 - 5 = " + sub(7, 5) + "\n4 * 3 = " + mul(4, 3));
+    div(25, 5);
+    div(5, 25);
   }
 
   private static int add(int num1, int num2) {
@@ -25,7 +20,11 @@ public class Calc {
     return num1 * num2;
   }
 
-  private static int div(int num1, int num2) {
-    return num1 / num2;
+  private static void div(double num1, double num2) {
+    if (num1 < num2) {
+      System.out.println(num1 + "/" + num2 + " = " + (num1 / num2));
+    } else {
+      System.out.println("Division cannot be performed");
+    }
   }
 }
--- com.qa.javaexamples/Results.java
+++ com.qa.javaexamples/Results.java
@@ -2,9 +2,9 @@ package com.qa.javaexamples;
 
 public class Results {
   public static void main(String[] args) {
-    int phys = 97;
-    int chem = 43;
-    int bio = 98;
+    int phys = 60;
+    int chem = 100;
+    int bio = 150;
     printResults(phys, chem, bio);
     calcPercentage(phys, chem, bio);
   }
@@ -26,5 +26,20 @@ public class Results {
     int p1 = (100 * (phys + chem + bio));
     double percent = (double) p1 / 450;
     System.out.println("Percent total mark: " + percent);
+    int fails = 0;
+    if (100 * (double) phys / 150 < 60) {
+      fails++;
+    }
+    if (100 * (double) chem / 150 < 60) {
+      fails++;
+    }
+    if (100 * (double) bio / 150 < 60) {
+      fails++;
+    }
+    if (fails > 0) {
+      System.out.println(fails + " module(s) failed");
+    } else {
+      System.out.println("All modules passed");
+    }
   }
 }
