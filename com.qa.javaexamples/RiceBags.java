655e546a8ef20a7e014740232690989fa3aa09e5
DavidBarringerQA <DBarringer@qa.com>
2021-06-23 16:37:42 +0100

    Added a method to find minimum small rice bags

--- com.qa.javaexamples/RiceBags.java
+++ com.qa.javaexamples/RiceBags.java
@@ -2,9 +2,9 @@ package com.qa.javaexamples;
 
 public class RiceBags {
   public static void main(String[] args) {
-    System.out.println(possible(22, 6, 5));
-    System.out.println(possible(13, 1, 3));
-    System.out.println(possible(16, 8, 2));
+    System.out.println(smallBagCount(22, 6, 5));
+    System.out.println(smallBagCount(13, 1, 3));
+    System.out.println(smallBagCount(16, 8, 2));
   }
 
   private static boolean possible(int goal, int small, int large) {
@@ -17,5 +17,17 @@ public class RiceBags {
       return false;
     }
     return true;
+  }
+
+  private static int smallBagCount(int goal, int small, int large) {
+    if (!possible(goal, small, large)) {
+      return -1;
+    }
+    if ((goal - (goal % 5)) / 5 > large) {
+      goal -= large * 5;
+    } else {
+      goal = goal % 5;
+    }
+    return goal;
   }
 }
