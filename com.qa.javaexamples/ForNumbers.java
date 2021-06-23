accb872e4f280e4f20e64fa5cbaef3a24655e95a
DavidBarringerQA <DBarringer@qa.com>
2021-06-23 17:10:30 +0100

    Updated ForNumbers to include parts I missed

--- com.qa.javaexamples/ForNumbers.java
+++ com.qa.javaexamples/ForNumbers.java
@@ -5,6 +5,18 @@ public class ForNumbers {
     for (int i = 1; i <= 100; i++) {
       System.out.println(printNum(i));
     }
+    System.out.println(printNum(142));
+    System.out.println(printNum(3000));
+    System.out.println(printNum(7777));
+  }
+
+  private static int digitAdd(int num) {
+    if (num < 10 || num >= 100) {
+      return -1;
+    }
+    int ones = num % 10;
+    int tens = (num - ones) / 10;
+    return ones + tens;
   }
 
   private static String smalls(int num) {
@@ -53,28 +65,38 @@ public class ForNumbers {
   }
 
   private static String printNum(int num) {
-    if (num <= 0) {
+    if (num < 0) {
       return "number out of scope";
     } else if (num < 20) {
       return smalls(num);
     } else if (num < 30) {
-      return "twenty " + smalls(num - 20);
+      return "twenty-" + smalls(num - 20);
     } else if (num < 40) {
-      return "thirty " + smalls(num - 30);
+      return "thirty-" + smalls(num - 30);
     } else if (num < 50) {
-      return "forty " + smalls(num - 40);
+      return "forty-" + smalls(num - 40);
     } else if (num < 60) {
-      return "fifty " + smalls(num - 50);
+      return "fifty-" + smalls(num - 50);
     } else if (num < 70) {
-      return "sixty " + smalls(num - 60);
+      return "sixty-" + smalls(num - 60);
     } else if (num < 80) {
-      return "seventy " + smalls(num - 70);
+      return "seventy-" + smalls(num - 70);
     } else if (num < 90) {
-      return "eighty " + smalls(num - 80);
+      return "eighty-" + smalls(num - 80);
     } else if (num < 100) {
-      return "ninety " + smalls(num - 90);
-    } else if (num == 100) {
-      return "one hundred";
+      return "ninety-" + smalls(num - 90);
+    } else if (num < 1000 && num % 100 == 0) {
+      return printNum(num / 100) + " hundred";
+    } else if (num < 1000) {
+      return printNum((num - (num % 100)) / 100)
+          + " hundred and "
+          + printNum(num - (num - (num % 100)));
+    } else if (num < 10000 && num % 1000 == 0) {
+      return printNum(num / 1000) + " thousand";
+    } else if (num < 10000) {
+      return printNum((num - (num % 1000)) / 1000)
+          + " thousand, "
+          + printNum(num - (num - (num % 1000)));
     } else {
       return "number out of scope";
     }
